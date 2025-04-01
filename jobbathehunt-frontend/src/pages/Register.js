bd76acf0f7b12391459e2d6372b1b9ca72920c89
Klarisse Anne Borlado <202311075@gordoncollege.edu.ph>
2025-04-01 13:45:28 +0800

    Update Register.js

--- jobbathehunt-frontend/src/pages/Register.js
+++ jobbathehunt-frontend/src/pages/Register.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "../css/Signup.css";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth } from "../firebase";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
