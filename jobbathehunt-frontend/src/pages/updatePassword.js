331a4b3a0efa75d3084c1785d526b9e9942e0b69
Ritchell-Malang <202310885@gordoncollege.edu.ph>
2025-04-12 14:23:07 +0800

    Update updatePassword.js

--- jobbathehunt-frontend/src/pages/updatePassword.js
+++ jobbathehunt-frontend/src/pages/updatePassword.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import { sendPasswordResetEmail } from "firebase/auth";
 import { auth } from "../firebase";
+import "../css/password.css"; 
+import { useNavigate } from "react-router-dom";
 
 
 function UpdatePassword() {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
   const [error, setError] = useState("");
+  const navigate = useNavigate();
 
   const handlePasswordReset = async () => {
     if (!email) {
@@ -31,23 +34,42 @@ function UpdatePassword() {
       }
     }
   };
+  const gotoLogin = () => {
+    navigate("/login");
+  };
 
   return (
-    <div className="update-password-container">
+    <div className="reset-password-container">
+       <div className="triangle-reset1"><img src="triangle sa bg.png" alt="bg1"/></div>
+       <div className="triangle-reset2"><img src="triangle sa bg1.png" alt="bg2"/></div>
+      <div className="reset-panel">
       <h1>Reset Your Password</h1>
       <p>Enter your email address to receive a password reset link.</p>
+      <div className="reset-line"></div>
+
+
+      <div className="reset-box">Email Address</div>
       <input
         type="email"
         placeholder="Email Address"
         value={email}
         onChange={(e) => setEmail(e.target.value.toLowerCase())}
-        className="input-box"
+        className="input-box-reset"
       />
+
+
+
       <button onClick={handlePasswordReset} className="reset-btn">
         Send Reset Email
       </button>
+      <div className="reset-msg">
       {message && <p className="success-message">{message}</p>}
       {error && <p className="error-message">{error}</p>}
+      </div>
+
+
+      <div className="reset-login">Remember Password?<span onClick={gotoLogin}> Login</span></div>
+      </div>
     </div>
   );
 }
